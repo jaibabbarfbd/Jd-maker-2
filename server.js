b699f1a0ee4f745d582faddf0b49f8526efd592e
jaibabbarfbd <jaibabbarfbd@gmail.com>
2026-07-12 22:41:17 +0530

    fixed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const app = express();
